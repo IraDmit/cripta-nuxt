543c89bce1908a43eb4dce5d8b1963f1c8b9144a
IraDmitrieva <iritrieva@gmail.com>
2023-07-15 18:31:44 +0400

    1

--- apis/api.js
+++ apis/api.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import axios from 'axios'
-
-export const loadTicker = async (tickers)=>{
-  console.log(tickers);
-  const { data } = await axios.get(
-    `https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=${tickers}&api_key=d580e57d5249755e75ac50f672d6a99a3b607971503abb8373efd9ecc6039ac6`
-  )
-  return data
-}
-=======
 // import axios from 'axios'
 const apiKey =
   'd580e57d5249755e75ac50f672d6a99a3b607971503abb8373efd9ecc6039ac6'
@@ -68,4 +57,3 @@ if (socket) {
     handler.forEach((func) => func(newPrice))
   })
 }
->>>>>>> fbe5b8f (ws iter 1)
